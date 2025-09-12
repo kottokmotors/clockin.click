617cbcbf9248e3ac139e7091f52ead1521057f4c
Lilith Prince <kottok.motors@gmail.com>
2025-09-12 11:32:15 -0500

    fix dns domain

--- terraform/modules/school/route53.tf
+++ terraform/modules/school/route53.tf
@@ -1,7 +1,7 @@
 # Route53 record pointing school subdomain to App Runner default URL
 resource "aws_route53_record" "school_subdomain" {
   zone_id = var.route53_zone_id
-  name    = "${var.school_id}.clockin"  # e.g., school1.clockin.click
+  name    = var.school_id  # e.g., school1.clockin.click
   type    = "CNAME"
   ttl     = 300
   records = [aws_apprunner_service.service.service_url]
